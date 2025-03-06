752a79d273a3e4328421377dbcbbc13271b5723b
kavin <kavin.ned@gmail.com>
2025-03-06 20:38:58 +0800

    install react-query

--- src/App.jsx
+++ src/App.jsx
@@ -14,10 +14,21 @@ import Login from "./pages/Login";
 import PageNotFound from "./pages/PageNotFound";
 import GlobalStyles from "./styles/GlobalStyles";
 import AppLayout from "./ui/AppLayout";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+const queryClient = new QueryClient({
+    defaultOptions: {
+        queries: {
+            staleTime: 60 * 1000,
+        },
+    },
+});
 
 export default function App() {
     return (
-        <>
+        <QueryClientProvider client={queryClient}>
+            <ReactQueryDevtools />
             <GlobalStyles />
             <Router>
                 <Routes>
@@ -37,6 +48,6 @@ export default function App() {
                     <Route path="*" element={<PageNotFound />} />
                 </Routes>
             </Router>
-        </>
+        </QueryClientProvider>
     );
 }
