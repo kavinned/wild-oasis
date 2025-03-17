bd1aad51b781d52aac1afa499b3bf9962d1d7220
kavin <kavin.ned@gmail.com>
2025-03-17 16:43:53 +0800

    add cabin creation form with validation and integrate react-hook-form

--- src/pages/Cabins.jsx
+++ src/pages/Cabins.jsx
@@ -1,8 +1,12 @@
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
 import CabinTable from "../features/cabins/CabinTable";
+import CreateCabinForm from "../features/cabins/CreateCabinForm";
+import Button from "../ui/Button";
+import { useState } from "react";
 
 function Cabins() {
+    const [showForm, setShowForm] = useState(false);
     return (
         <>
             <Row type="horizontal">
@@ -11,6 +15,10 @@ function Cabins() {
             </Row>
             <Row>
                 <CabinTable />
+                <Button onClick={() => setShowForm(!showForm)}>
+                    {showForm ? "Close" : "Add new cabin"}
+                </Button>
+                {showForm && <CreateCabinForm />}
             </Row>
         </>
     );
