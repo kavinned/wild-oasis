3fefc2fbcb57cc01a3e568dc67eb7b05a6402247
kavin <kavin.ned@gmail.com>
2025-03-21 01:03:16 +0800

    refactor cabin management by extracting custom hooks for create, edit, and delete operations

--- src/features/cabins/CabinRow.jsx
+++ src/features/cabins/CabinRow.jsx
@@ -1,10 +1,8 @@
 import styled from "styled-components";
 import { formatCurrency } from "../../utils/helpers";
-import { useMutation, useQueryClient } from "@tanstack/react-query";
-import { deleteCabin } from "../../services/apiCabins";
-import toast from "react-hot-toast";
 import { useState } from "react";
 import CreateCabinForm from "../../features/cabins/CreateCabinForm";
+import useDeleteCabin from "./useDeleteCabin";
 
 const TableRow = styled.div`
     display: grid;
@@ -57,18 +55,7 @@ export default function CabinRow({ cabin }) {
         image,
     } = cabin;
 
-    const queryClient = useQueryClient();
-
-    const { isLoading: isDeleting, mutate } = useMutation({
-        mutationFn: deleteCabin,
-        onSuccess: () => {
-            toast.success("Cabin succesfully deleted");
-            queryClient.invalidateQueries({
-                queryKey: ["cabins"],
-            });
-        },
-        onError: (err) => toast.error(err.message),
-    });
+    const { isDeleting, deleteCabin } = useDeleteCabin();
 
     return (
         <>
@@ -77,12 +64,16 @@ export default function CabinRow({ cabin }) {
                 <Cabin>{name}</Cabin>
                 <div>Fits up to {maxCapacity}</div>
                 <Price>{formatCurrency(regularPrice)}</Price>
-                <Discount>{formatCurrency(discount)}</Discount>
+                {discount ? (
+                    <Discount>{formatCurrency(discount)}</Discount>
+                ) : (
+                    <span>&mdash;</span>
+                )}
                 <div>
                     <button onClick={() => setShowForm(!showForm)}>Edit</button>
                     <button
                         disabled={isDeleting}
-                        onClick={() => mutate(cabinId)}
+                        onClick={() => deleteCabin(cabinId)}
                     >
                         Delete
                     </button>
--- src/features/cabins/CreateCabinForm.jsx
+++ src/features/cabins/CreateCabinForm.jsx
@@ -1,13 +1,12 @@
 import { useForm } from "react-hook-form";
-import { useMutation, useQueryClient } from "@tanstack/react-query";
-import { createEditCabin } from "../../services/apiCabins";
-import toast from "react-hot-toast";
 import Input from "../../ui/Input";
 import Form from "../../ui/Form";
 import Button from "../../ui/Button";
 import FileInput from "../../ui/FileInput";
 import Textarea from "../../ui/Textarea";
 import FormRow from "../../ui/FormRow";
+import useCreateCabin from "./useCreateCabin";
+import useEditCabin from "./useEditCabin";
 
 function CreateCabinForm({ cabinToEdit = {} }) {
     const { id: editId, ...editValues } = cabinToEdit;
@@ -16,35 +15,9 @@ function CreateCabinForm({ cabinToEdit = {} }) {
         defaultValues: isEditSession ? editValues : {},
     });
     const { errors } = formState;
-    const queryClient = useQueryClient();
 
-    const { mutate: createCabin, isLoading: isCreating } = useMutation({
-        mutationFn: createEditCabin,
-        onSuccess: () => {
-            toast.success("New cabin successfully created");
-            queryClient.invalidateQueries({
-                queryKey: ["cabins"],
-            });
-            reset();
-        },
-        onError: (err) => {
-            toast.error(err.message);
-        },
-    });
-
-    const { mutate: editCabin, isLoading: isEditing } = useMutation({
-        mutationFn: ({ newCabinData, id }) => createEditCabin(newCabinData, id),
-        onSuccess: () => {
-            toast.success("Cabin successfully edited");
-            queryClient.invalidateQueries({
-                queryKey: ["cabins"],
-            });
-            reset();
-        },
-        onError: (err) => {
-            toast.error(err.message);
-        },
-    });
+    const { isCreating, createCabin } = useCreateCabin();
+    const { isEditing, editCabin } = useEditCabin();
 
     const isWorking = isCreating || isEditing;
 
@@ -54,9 +27,25 @@ function CreateCabinForm({ cabinToEdit = {} }) {
             typeof data.image === "string" ? data.image : data.image[0];
 
         if (isEditSession) {
-            editCabin({ newCabinData: { ...data, image }, id: editId });
+            editCabin(
+                { newCabinData: { ...data, image }, id: editId },
+                {
+                    onSuccess: (data) => {
+                        reset();
+                        console.log(data);
+                    },
+                }
+            );
         } else {
-            createCabin({ ...data, image });
+            createCabin(
+                { ...data, image },
+                {
+                    onSuccess: (data) => {
+                        reset();
+                        console.log(data);
+                    },
+                }
+            );
         }
     }
 
@@ -117,7 +106,6 @@ function CreateCabinForm({ cabinToEdit = {} }) {
                 <Input
                     type="number"
                     id="discount"
-                    disabled={isWorking}
                     defaultValue={0}
                     {...register("discount", {
                         required: "This field is required",
